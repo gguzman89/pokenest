2a0b4381f2d6bd50ffa83a8cbf045df6f0cc89f4
gabriel guzman <ggtpi0gabriel@gmail.com>
2026-04-19 14:46:02 -0300

    feat: initial proyect w/ static serve
     Resolve Conflicts:
    	README.md

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { join } from 'path'; // en Node
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    })
+  ],
+
+})
+export class AppModule { }
