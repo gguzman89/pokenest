deda8377600b954263a77f1559bce0340f53c8c8
gabriel guzman <ggtpi0gabriel@gmail.com>
2026-04-22 14:25:11 -0300

    feat: adapter common w/ interface - Axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
-import { PokeResponse } from './interfaces/poke-response.interface';
-import { PokemonService } from 'src/pokemon/pokemon.service';
 import { InjectModel } from '@nestjs/mongoose';
-import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { PokemonService } from 'src/pokemon/pokemon.service';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
 
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
   ) { }
 
   async executeSeed() {
 
     await this.pokemonModel.deleteMany({}); // delete * from Pokemon
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
 
     const pokemonToInsert: { name: string, no: number }[] = [];
 
