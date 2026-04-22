88538172730ad939795c9952a29ed11a6a12d6d2
gabriel guzman <ggtpi0gabriel@gmail.com>
2026-04-22 12:55:05 -0300

    feat: insert many Pokemon at DB

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,25 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) { }
+
   async executeSeed() {
 
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
 
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
-      console.log({ name, no });
-
+      const pokemon = await this.pokemonModel.create({ name, no }) // resolution FH
     })
 
-    return data.results;
+    // pokemons.forEach(async pokemon => await this.pokemonService.create(pokemon)) // Mi opcion
+    return 'Seed executed';
+
   }
 }
